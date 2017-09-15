9a66101a564eb3f640cffc0156d916f77dd29eb0
Mohsin Ghani <mohsinghani.777@gmail.com>
2017-09-15 03:59:06 -0700

    implement automatic scroll down messages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild  } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 import { Login } from '../login/login'
 import { UsersService } from '../../providers/users-service'
+import { Content } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Content) content: Content;
+
   [x: string]: any;
   message: string;
   currentUser;
@@ -19,12 +23,19 @@ export class HomePage {
   month = new Date().getMonth()
   year = new Date().getFullYear()
   currentDate = `${this.day}-${this.month}-${this.year}`
-
+  
   constructor(public navCtrl: NavController,private _AngularFireDatabase: AngularFireDatabase, public _UsersService: UsersService) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser')) || null;
     this.userKey = localStorage.getItem('userKey')
     this.conversation = this._AngularFireDatabase.list(`/conversation/${this.userKey}`);
+    this.conversation.subscribe(()=>{
+      //for scroll down
+      setTimeout(() => {
+        this.content.scrollToBottom();
+      }, 0);
+    })
   }
+  
 
   userLogout(){
     this._UsersService.logoutUser().then(() => {
@@ -41,7 +52,8 @@ export class HomePage {
 
   sendMessage(){
     let send = { name: this.currentUser.name, imageUrl: '../assets/images/user.png', text: this.message }
-    this._AngularFireDatabase.list(`/conversation/${this.userKey}`).push(send);
+    this._AngularFireDatabase.list(`/conversation/${this.userKey}`).push(send).then(()=>{
+    });
     this.message = '';
   }
 
